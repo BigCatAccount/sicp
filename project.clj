83a1329a4c2dc6cea6e2d233f76e6ce8861068b4
MPechorin <maksimp@wix.com>
2015-04-02 21:18:38 +0300

    midje - fixing clojure naming

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.pechorin/sicp "0.1.0-SNAPSHOT"
   :description "SICP course and book exercises"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  :profiles {:dev {:dependencies [[midje "1.6.3" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-midje "3.1.3"]]}}
   :test-paths ["test"]
   :source-paths ["src"])
--- test/temp_test.clj
+++ test/temp_test.clj
@@ -1,7 +1,7 @@
-(ns temp
+(ns temp-test
   (:use temp
         midje.sweet))
 
 (facts "my-sum"
-  (my-sum []) => -1
+  (my-sum []) => 0
   (my-sum [1 2 3]) => 6)
